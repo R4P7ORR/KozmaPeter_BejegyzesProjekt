4b1770c927928a965ea1184a53a13071694136e9
R4P7ORR <peetya.77@gmail.com>
2023-09-26 09:21:02 +0200

    Bejegyzés szám bekérése

--- Program.cs
+++ Program.cs
@@ -8,9 +8,25 @@ namespace BejegyzesProjekt
 {
     internal class Program
     {
+        static List<Bejegyzes> bList1 = new List<Bejegyzes>();
+        static List<Bejegyzes> bList2 = new List<Bejegyzes>();
+
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Kérem adjon meg egy számot! (Ennyi új bejegyzés fog készülni.)");
+            int bejegyzesek = 0;
+            while (!int.TryParse(Console.ReadLine().ToLower(), out bejegyzesek))
+			{
+				Console.WriteLine("Nem megfelelő a megadott érték! (CSAK egész számot adhat meg!)");
+			}
+            for (int i = 0; i < bejegyzesek; i++)
+			{
+                Console.WriteLine("Kérem adja meg a szerző nevét, majd a bejegyzéshez kívánt tartalmat!");
+                string szerzo = Console.ReadLine();
+                string tartalom = Console.ReadLine();
+                Bejegyzes b = new Bejegyzes(szerzo,tartalom);
+                bList1.Add(b);
+			}
 
             Console.ReadKey();
         }
